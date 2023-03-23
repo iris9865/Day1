3d331925754348aadeb18d117c0afd127e62718e
iris9865 <126546296+iris9865@users.noreply.github.com>
2023-03-23 12:46:55 +0900

    Update custom.js

--- responsive web page/custom.js
+++ responsive web page/custom.js
@@ -29,5 +29,14 @@ $(function() {
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 2
-  })
+  });
+  $('.project-photo').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: 'linear'
+  });
 });
