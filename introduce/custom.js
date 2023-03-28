c0c4936bae80bf1fc2aecdf5c3d93a0fa77635c3
iris9865 <126546296+iris9865@users.noreply.github.com>
2023-03-28 18:22:04 +0900

    Add files via upload

---
+++ introduce/custom.js
@@ -0,0 +1,17 @@
+$(function() {
+  // Slick Slider 
+  $('.slider-mobile').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+  $('.slider-webpage').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+});
