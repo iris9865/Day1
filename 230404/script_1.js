e39b61c7a92e7c9d96fb17bc481ee81f639e5127
iris9865 <126546296+iris9865@users.noreply.github.com>
2023-04-04 18:24:15 +0900

    Add files via upload

---
+++ 230404/script_1.js
@@ -0,0 +1,10 @@
+let pic = document.querySelector('#pic');
+pic.addEventListener("mouseover", changePic);
+pic.addEventListener("mouseout", originPic);
+
+function changePic() {
+  pic.src = "/images/boy.png";
+}
+function originPic() {
+  pic.src = "/images/girl.png";
+}
